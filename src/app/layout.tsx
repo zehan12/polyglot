65de7665fae21f0d28115013d58a9b7dfc2000f9
Zehan Khan <zkhan@hootboard.com>
2024-03-17 01:18:06 +0530

    feat: :cop: add clerk provider and middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@
 |
 *-------------------------------------------------------------------*/
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
@@ -29,8 +30,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
